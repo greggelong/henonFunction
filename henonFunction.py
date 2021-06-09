0c8c938d8a8e1ae69dfcc08bb0df8c4e554a8a46
Greg Kreisman <gkreisman_cep@qq.com>
2021-06-09 21:00:18 +0800

    Add files via upload

---
+++ henonFunction.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def henon(a=1.4,b=0.3,x=0.2,y=0.2):
+    global lx,ly
+
+    for i in range(1,3000):
+        xnew = 1+y-a*x**2
+        ynew = b*x
+        #print(i,xnew,ynew)
+        lx.append(xnew)
+        ly.append(ynew)
+        
+        x =xnew
+        y =ynew
+    
+    
+# initalize lists for plotting
+lx = []
+ly = []
+
+#henon(0.8,0.3,0.2,0.2) #henon(a,b,x,y)
+henon()
+
+plt.scatter(lx,ly, marker=".", alpha=0.2)
+plt.show()
